99289bc9c2eeb963efed28011de69b47ff236c3f
Jignesh <jigneshdinesh.madhani@sjsu.edu>
2020-05-23 06:31:43 -0700

    Test file

---
+++ email-alert/src/test/java/com/emailalert/EmailAlertApplicationTests.java
@@ -0,0 +1,12 @@
+package com.emailalert;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAlertApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
