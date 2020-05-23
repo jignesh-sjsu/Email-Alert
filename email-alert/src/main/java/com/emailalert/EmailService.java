db424c21c005d7be25538c3894a5ab1374350336
Jignesh <jigneshdinesh.madhani@sjsu.edu>
2020-05-23 06:21:57 -0700

    Email service for template configuration

---
+++ email-alert/src/main/java/com/emailalert/EmailService.java
@@ -0,0 +1,36 @@
+package com.emailalert;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    public void sendMail(String sender, String receiver, String subject, String message, String templateName) {
+        Mail mail = new Mail();
+        mail.setMailFrom(sender);
+        mail.setMailTo(receiver);
+        mail.setMailSubject(subject);
+        mail.setMailContent(message);
+        mail.setTemplateName(templateName);
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        Mailer mailer = (Mailer) context.getBean("mailer");
+        mailer.sendCustomMail(mail);
+    }
+
+    public void sendJobMail(String sender, String receiver, ScheduleJob job) {
+        Mail mail = new Mail();
+        mail.setMailFrom(sender);
+        mail.setMailTo(receiver);
+        mail.setMailSubject( job.getJobName() + " " + job.getStatus().getStatus());
+        if (JobStatus.COMPLETED == job.getStatus()) {
+            mail.setTemplateName("confirmed-template.vm");
+        } else {
+            mail.setTemplateName("failed-template.vm");
+        }
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        Mailer mailer = (Mailer) context.getBean("mailer");
+        mailer.sendJobMail(mail, job);
+    }
+}
