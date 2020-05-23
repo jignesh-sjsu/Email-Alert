37bf39be13294350e135d0df141caf1057507a71
Jignesh <jigneshdinesh.madhani@sjsu.edu>
2020-05-23 06:19:40 -0700

    CommonConstant to hold sender and receiver email id

---
+++ email-alert/src/main/java/com/emailalert/CommonConstant.java
@@ -0,0 +1,6 @@
+package com.emailalert;
+
+public class CommonConstant {
+    public static final String SENDER = "swe.dev.20@gmail.com";
+    public static final String RECEIVER = "swe.dev.20@gmail.com";
+}
